5a24b2e033c3e5710b7669b83cd95b170a94e1eb
Scott Aubrey <scott@aubrey.org.uk>
2022-08-26 11:12:31 +0100

    EPP preview environments (#470)
    * design a script that queries open PRS and generates flux kustomizations for them
    
    * add a github action that can be triggered manually for now to build preview environments

---
+++ scripts/merge_epp_preview_envs_with_open_prs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+ENV_DEST_DIR='deployments/epp/previews'
+REPO='elifesciences/enhanced-preprints-server'
+KUSTOMIZATION_TEMPLATE='kustomizations/apps/epp/preview_template.yaml'
+
+# First, remove all envs. They will be recreated, and there's no race issues because they will be in a single commit, which is atomic
+rm $ENV_DEST_DIR/*.yaml
+
+# now create all envs related to current open and labelled PRs
+for pr in $(gh pr list --repo $REPO --label preview --json number,potentialMergeCommit,mergeable | jq -c '.[]'); do
+    pr_mergable="$(echo $pr | jq .mergeable)"
+
+    export pr_id="$(echo $pr | jq .number)"
+    export pr_commit="$(echo $pr | jq -r .potentialMergeCommit.oid)"
+    export image_tag="preview-${pr_commit:0:8}"
+
+    echo "create env for $(echo $pr | jq .number)"
+
+    envsubst < $KUSTOMIZATION_TEMPLATE > ${ENV_DEST_DIR}/${pr_id}.yaml
+done
+
+# now commit
+# Done in script, so that if there is an error, the whole script exits (set -e), and nothing is committed.
+git add $ENV_DEST_DIR
+#git commit
