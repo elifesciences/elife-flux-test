25a0aa2958598c99d88f7cb7de08a10e85177b65
Scott Aubrey <scott@aubrey.org.uk>
2023-04-24 17:17:33 +0100

    make the kind script actually work

--- scripts/run_cluster_in_kind.sh
+++ scripts/run_cluster_in_kind.sh
@@ -1,54 +1,48 @@
 #!/bin/bash
 set -e
 
-# comment this line to run commands, uncomment to echo them
-# DEBUG="echo "
 
-$DEBUG kind delete cluster --name "elife-flux-test"
-$DEBUG kind create cluster --name "elife-flux-test" --image=kindest/node:v1.25.8 --wait 30s
+# if a param is specified, update the flux-system source branch to that
+branch=${1:-master}
+
+echo "running cluster on '$branch' branch"
+exit 0
+
+kind delete cluster --name "elife-flux-test"
+kind create cluster --name "elife-flux-test" --image=kindest/node:v1.25.8 --wait 30s
 
 # install kwok into cluster
-$DEBUG kubectl kustomize scripts/kwok/deploy_config | kubectl apply -f -
-$DEBUG kubectl wait deployment -n kube-system kwok-controller --timeout=30s --for condition=Available=True
+kubectl kustomize scripts/kwok/deploy_config | kubectl apply -f -
+kubectl wait deployment -n kube-system kwok-controller --timeout=30s --for condition=Available=True
 
 # Install Flux with toleration to run controllers on the real node
-$DEBUG kubectl create ns flux
+kubectl create ns flux
 # make sure flux components get installed, with additional components
-$DEBUG flux install --components-extra="image-reflector-controller,image-automation-controller" --toleration-keys=realnode
+flux install --components-extra="image-reflector-controller,image-automation-controller" --toleration-keys=realnode
 
 # taint the current node to not schedule workloads by default
-$DEBUG kubectl taint node elife-flux-test-control-plane realnode=true:NoSchedule
+kubectl taint node elife-flux-test-control-plane realnode=true:NoSchedule
 # Install kwok nodes to run "workloads" on
-$DEBUG kubectl apply -f scripts/kwok/1_large_simulated_node.yaml
+kubectl apply -f scripts/kwok/1_large_simulated_node.yaml
 
 
 
 # Install cluster stuff and wait
-$DEBUG flux create source git flux-system --url=https://github.com/elifesciences/elife-flux-test --branch=master
-$DEBUG flux create kustomization flux-system --source=flux-system --path=./clusters/end-to-end-tests
-$DEBUG kubectl wait kustomizations.kustomize.toolkit.fluxcd.io --for=condition=ready --timeout=10m -n flux-system crds
-$DEBUG kubectl wait kustomizations.kustomize.toolkit.fluxcd.io --for=condition=ready --timeout=10m -n flux-system system
-$DEBUG kubectl wait kustomizations.kustomize.toolkit.fluxcd.io --for=condition=ready --timeout=10m -n flux-system deployments
+flux create source git flux-system --url=https://github.com/elifesciences/elife-flux-test --branch=master
+flux create kustomization flux-system --source=flux-system --path=./clusters/end-to-end-tests
+kubectl wait kustomizations.kustomize.toolkit.fluxcd.io --for=condition=ready --timeout=10m -n flux-system crds
+kubectl wait kustomizations.kustomize.toolkit.fluxcd.io --for=condition=ready --timeout=10m -n flux-system system
+kubectl wait kustomizations.kustomize.toolkit.fluxcd.io --for=condition=ready --timeout=10m -n flux-system deployments
 
-$DEBUG kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n autoscaler         cluster-autoscaler
-$DEBUG kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n infra             sealed-secrets
-$DEBUG kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n infra              ingress-nginx
-$DEBUG kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n infra              cert-manager
-$DEBUG kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n infra              external-dns
-$DEBUG kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n infra              oauth2-proxy
-$DEBUG kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n kube-system        descheduler
-$DEBUG kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n monitoring         metrics-server
-# $DEBUG kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n monitoring         gitops-dashboard
-$DEBUG kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n logging            loki-stack
-$DEBUG kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n crossplane-system  crossplane
-$DEBUG kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n db-operator-system psmdb-operator
-$DEBUG kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=10m -n monitoring         prometheus-stack
-
-
-# if a param is specified, update the flux-system source branch to that
-if [ ! -z "$1"  ] && git rev-parse --verify $1; then
-    $DEBUG echo "About to do a sync from master to $1 branch. "
-
-    $DEBUG flux create source git flux-system   --url=https://github.com/elifesciences/elife-flux-test   --branch=$1
-    $DEBUG flux reconcile source git flux-system
-fi;
+kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n autoscaler         cluster-autoscaler
+kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n infra             sealed-secrets
+kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n infra              ingress-nginx
+kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n infra              cert-manager
+kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n infra              external-dns
+kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n infra              oauth2-proxy
+kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n kube-system        descheduler
+kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n monitoring         metrics-server
+kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n logging            loki-stack
+kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n crossplane-system  crossplane
+kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=5m -n db-operator-system psmdb-operator
+kubectl wait helmreleases.helm.toolkit.fluxcd.io --for=condition=ready --timeout=10m -n monitoring         prometheus-stack
