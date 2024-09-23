991ac362b54e3b503ac353e468bbd9c2c8cdf685
Scott Aubrey <scott@aubrey.org.uk>
2024-09-23 12:45:12 +0100

    deploy(crds): create update script and update cert-manager CRDs elifesciences/issues#8934

---
+++ crds/cert-manager/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# As script to update these manually
+
+if [ -z "$1" ]; then
+    echo 'ERROR: specify a cert-manager release version to get the CRDs'
+    echo "e.g. \`$0 v1.15.3\`"
+    exit 1
+fi
+
+cd $(dirname $0)
+
+curl -L https://github.com/cert-manager/cert-manager/releases/download/$1/cert-manager.crds.yaml > cert-manager.crd.yaml
