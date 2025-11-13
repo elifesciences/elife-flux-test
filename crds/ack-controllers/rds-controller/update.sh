8ee4e439d2c95fef5241b5ac777f812dd91096d9
Scott Aubrey <scott@aubrey.org.uk>
2025-11-13 07:18:24 +0000

    deploy(crds/ack-controllers): Update CRDs, and do a general refresh of all ack crds after #2296 #2295 #2297 #2298 #2294 #2292 #2299

--- crds/ack-controllers/rds-controller/update.sh
+++ crds/ack-controllers/rds-controller/update.sh
@@ -10,8 +10,10 @@ fi
 cd $(dirname $0)
 
 
-curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/rds-controller/refs/tags/v$1/config/crd/bases/rds.services.k8s.aws_dbclusters.yaml
+# Adopted from https://github.com/aws-controllers-k8s/rds-controller/tree/main/config/crd/bases
+curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/rds-controller/refs/tags/v$1/config/crd/bases/rds.services.k8s.aws_dbclusterendpoints.yaml
 curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/rds-controller/refs/tags/v$1/config/crd/bases/rds.services.k8s.aws_dbclusterparametergroups.yaml
+curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/rds-controller/refs/tags/v$1/config/crd/bases/rds.services.k8s.aws_dbclusters.yaml
 curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/rds-controller/refs/tags/v$1/config/crd/bases/rds.services.k8s.aws_dbclustersnapshots.yaml
 curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/rds-controller/refs/tags/v$1/config/crd/bases/rds.services.k8s.aws_dbinstances.yaml
 curl -sO https://raw.githubusercontent.com/aws-controllers-k8s/rds-controller/refs/tags/v$1/config/crd/bases/rds.services.k8s.aws_dbparametergroups.yaml
