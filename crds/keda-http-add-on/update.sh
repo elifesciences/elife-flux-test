5c2fe81963be785bd2dcedeb4619177b29226454
Daniel Haarhoff <daniel@rknt.de>
2026-04-28 12:05:15 +0100

    chore(keda-http): Use CRDs from GH releases as new version are available from there

--- crds/keda-http-add-on/update.sh
+++ crds/keda-http-add-on/update.sh
@@ -3,11 +3,11 @@
 # As script to update these manually
 
 if [ -z "$1" ]; then
-    echo 'ERROR: specify a keda-http-add-on release version to get the CRDs'
-    echo "e.g. \`$0 0.7.0\`"
-    exit 1
+  echo 'ERROR: specify a keda-http-add-on release version to get the CRDs'
+  echo "e.g. \`$0 0.7.0\`"
+  exit 1
 fi
 
 cd $(dirname $0)
 
-curl -sL https://raw.githubusercontent.com/kedacore/http-add-on/refs/tags/v$1/config/crd/bases/http.keda.sh_httpscaledobjects.yaml > keda-http-add-on.crd.yaml
+curl -sL https://github.com/kedacore/http-add-on/releases/download/v$1/keda-add-ons-http-$1-crds.yaml >keda-http-add-on.crd.yaml
